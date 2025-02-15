405549ecdc1ebe6bb57081fa2e4f8612124eae4b
abdulrehan17773 <theroyal.info4u@gmail.com>
2025-02-15 19:27:14 +0500

    add pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,20 @@
 import Home from "./Home";
 import Cart from "./Cart";
 import Login from "./Login";
+import Orders from "./Orders";
+import OrderDetails from "./OrderDetails";
+import Signup from "./Signup";
+import ForgetPassword from "./ForgetPassword";
+import Search from "./Search"
+
 
 export{
     Home,
     Cart,
-    Login
+    Login,
+    Orders,
+    OrderDetails,
+    Signup,
+    ForgetPassword,
+    Search
 }
